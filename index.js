4ebf8956df825a763167edfdd8faa6e67e268e5d
Ananthu Kanive <ananthu.kanive@smallcase.com>
2020-09-10 19:26:28 +0530

    add js interface with sample app

--- index.js
+++ index.js
@@ -1,5 +1,43 @@
-import { NativeModules } from 'react-native';
+import { NativeModules, Platform } from "react-native";
 
-const { RNBiometrics } = NativeModules;
+const { RNBiometrics: RNBiometricsNative } = NativeModules;
+
+/**
+ * check if authentication is possible
+ *
+ * @returns {Promise<boolean>} result
+ */
+const canAuthenticate = () => {
+  if (Platform.OS === "ios") {
+    // no op for ios
+    return Promise.resolve(true);
+  }
+
+  return RNBiometricsNative.canAuthenticate();
+};
+
+/**
+ * request biometric authentication
+ *
+ * note: promise will resolve when successful
+ * but will be rejected when not with an error message
+ *
+ * @param {string} title
+ * @param {string} subtitle
+ * @returns {Promise<boolean>} result
+ */
+const requestBioAuth = (title, subtitle) => {
+  if (Platform.OS === "ios") {
+    // no op for ios
+    return Promise.resolve(true);
+  }
+
+  return RNBiometricsNative.requestBioAuth(title || "", subtitle || "");
+};
+
+const RNBiometrics = {
+  requestBioAuth,
+  canAuthenticate,
+};
 
 export default RNBiometrics;
