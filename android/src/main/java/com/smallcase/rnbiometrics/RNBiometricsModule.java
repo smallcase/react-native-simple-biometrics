8a642963674f80a59166d46e8172e1b8ab78692a
Ananthu Kanive <ananthu.kanive@smallcase.com>
2020-09-10 19:26:08 +0530

    add android bridge methods

--- android/src/main/java/com/smallcase/rnbiometrics/RNBiometricsModule.java
+++ android/src/main/java/com/smallcase/rnbiometrics/RNBiometricsModule.java
@@ -1,17 +1,26 @@
 package com.smallcase.rnbiometrics;
 
+import android.app.Activity;
+
+import androidx.annotation.NonNull;
+import androidx.biometric.BiometricManager;
+import androidx.biometric.BiometricPrompt;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.FragmentActivity;
+
+import com.facebook.react.bridge.Callback;
+import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
-import com.facebook.react.bridge.Callback;
+
+import java.util.concurrent.Executor;
 
 public class RNBiometricsModule extends ReactContextBaseJavaModule {
 
-    private final ReactApplicationContext reactContext;
 
     public RNBiometricsModule(ReactApplicationContext reactContext) {
         super(reactContext);
-        this.reactContext = reactContext;
     }
 
     @Override
@@ -20,8 +29,56 @@ public class RNBiometricsModule extends ReactContextBaseJavaModule {
     }
 
     @ReactMethod
-    public void sampleMethod(String stringArgument, int numberArgument, Callback callback) {
-        // TODO: Implement some actually useful functionality
-        callback.invoke("Received numberArgument: " + numberArgument + " stringArgument: " + stringArgument);
+    public void canAuthenticate(Promise promise) {
+        try {
+            ReactApplicationContext context = getReactApplicationContext();
+            BiometricManager biometricManager = BiometricManager.from(context);
+            boolean can = biometricManager.canAuthenticate() == BiometricManager.BIOMETRIC_SUCCESS;
+            promise.resolve(can);
+        } catch (Exception e) {
+            promise.reject(e);
+        }
+    }
+
+    @ReactMethod
+    public void requestBioAuth(String title, String subtitle, final Promise promise) {
+        try {
+            ReactApplicationContext context = getReactApplicationContext();
+            Activity activity = getCurrentActivity();
+
+            Executor mainExecutor = ContextCompat.getMainExecutor(context);
+
+            final BiometricPrompt.AuthenticationCallback authenticationCallback = new BiometricPrompt.AuthenticationCallback() {
+
+                @Override
+                public void onAuthenticationError(int errorCode, @NonNull CharSequence errString) {
+                    super.onAuthenticationError(errorCode, errString);
+                    promise.reject(new Exception(errString.toString()));
+                }
+
+                @Override
+                public void onAuthenticationSucceeded(@NonNull BiometricPrompt.AuthenticationResult result) {
+                    super.onAuthenticationSucceeded(result);
+                    promise.resolve(true);
+                }
+            };
+
+            if(activity != null){
+                BiometricPrompt prompt = new BiometricPrompt((FragmentActivity) activity, mainExecutor, authenticationCallback);
+
+                BiometricPrompt.PromptInfo promptInfo = new BiometricPrompt.PromptInfo.Builder()
+                        .setTitle(title)
+                        .setSubtitle(subtitle)
+                        .setDeviceCredentialAllowed(true)
+                        .build();
+
+                prompt.authenticate(promptInfo);
+            } else {
+                throw new Exception("null activity");
+            }
+
+        } catch (Exception e) {
+            promise.reject(e);
+        }
     }
 }
