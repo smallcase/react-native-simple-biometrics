06e70e0b6f432f4f912ba4ef1d74139d81fbd078
Ananthu Kanive <ananthu.kanive@smallcase.com>
2022-07-18 09:54:58 +0530

    refactor: update ios files to latest guidelines

---
+++ ios/SimpleBiometrics.h
@@ -0,0 +1,5 @@
+#import <React/RCTBridgeModule.h>
+
+@interface SimpleBiometrics : NSObject <RCTBridgeModule>
+
+@end
--- ios/SimpleBiometrics.mm
+++ ios/SimpleBiometrics.mm
@@ -1,9 +1,13 @@
-#import "RNBiometrics.h"
+#import "SimpleBiometrics.h"
+
 #import <LocalAuthentication/LocalAuthentication.h>
 #import <React/RCTConvert.h>
 
-@implementation RNBiometrics
+#ifdef RCT_NEW_ARCH_ENABLED
+#import "RNSimpleBiometricsSpec.h"
+#endif
 
+@implementation SimpleBiometrics
 RCT_EXPORT_MODULE()
 
 RCT_REMAP_METHOD(canAuthenticate,
@@ -13,7 +17,7 @@ RCT_REMAP_METHOD(canAuthenticate,
     LAContext *context = [[LAContext alloc] init];
     NSError *la_error = nil;
     BOOL canEvaluatePolicy = [context canEvaluatePolicy:LAPolicyDeviceOwnerAuthenticationWithBiometrics error:&la_error];
-    
+
     if (canEvaluatePolicy) {
         resolve(@(YES));
     } else {
@@ -27,29 +31,38 @@ RCT_REMAP_METHOD(requestBioAuth,
                  requestBioAuthWithResolver:(RCTPromiseResolveBlock)resolve
                  rejecter:(RCTPromiseRejectBlock)reject)
 {
-    
+
     dispatch_async(dispatch_get_global_queue( DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
         NSString *promptMessage = subtitle;
-        
+
         LAContext *context = [[LAContext alloc] init];
         context.localizedFallbackTitle = title;
-        
+
         LAPolicy localAuthPolicy = LAPolicyDeviceOwnerAuthenticationWithBiometrics;
         if (![[UIDevice currentDevice].systemVersion hasPrefix:@"8."]) {
             localAuthPolicy = LAPolicyDeviceOwnerAuthentication;
         }
-        
+
         [context evaluatePolicy:localAuthPolicy localizedReason:promptMessage reply:^(BOOL success, NSError *biometricError) {
             if (success) {
                 resolve( @(YES));
-                
+
             } else {
                 NSString *message = [NSString stringWithFormat:@"%@", biometricError.localizedDescription];
                 reject(@"biometric_error", message, nil);
             }
         }];
     });
-    
+
 }
+
+// Don't compile this code when we build for the old architecture.
+#ifdef RCT_NEW_ARCH_ENABLED
+- (std::shared_ptr<facebook::react::TurboModule>)getTurboModule:
+    (const facebook::react::ObjCTurboModule::InitParams &)params
+{
+    return std::make_shared<facebook::react::NativeSimpleBiometricsSpecJSI>(params);
+}
+#endif
 
 @end
