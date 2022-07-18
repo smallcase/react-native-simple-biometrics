b7012d09d4308c273f97a51d43b286d055ee81d0
Ananthu Kanive <ananthu.kanive@smallcase.com>
2022-07-18 21:26:49 +0530

    feat: convert js source to ts

---
+++ src/index.tsx
@@ -0,0 +1,40 @@
+import { NativeModules } from 'react-native';
+
+const { SimpleBiometrics: RNBiometricsNative } = NativeModules;
+
+/**
+ * check if authentication is possible
+ */
+const canAuthenticate = (): Promise<boolean> => {
+  return RNBiometricsNative.canAuthenticate();
+};
+
+/**
+ * request biometric authentication
+ *
+ * note: promise will resolve when successful
+ * but will be rejected when not with an error message
+ */
+const requestBioAuth = (
+  /** title of prompt (can be an empty string) */
+  promptTitle: string,
+  /** The app-provided reason for requesting authentication, which displays in the authentication dialog presented to the user. */
+  promptMessage: string
+): Promise<boolean> => {
+  if (typeof promptTitle !== 'string' || !promptTitle) {
+    throw new Error('prompt title must be a non empty string');
+  }
+
+  if (typeof promptMessage !== 'string' || !promptMessage) {
+    throw new Error('prompt message must be a non empty string');
+  }
+
+  return RNBiometricsNative.requestBioAuth(promptTitle, promptMessage);
+};
+
+const RNBiometrics = {
+  requestBioAuth,
+  canAuthenticate,
+};
+
+export default RNBiometrics;
