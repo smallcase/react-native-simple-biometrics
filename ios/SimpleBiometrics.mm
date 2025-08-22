07c0d48dc626d67ea81fefcb282d65fd43b70d5e
Nivedita Singh <niveditasingh0627@gmail.com>
2025-08-22 13:23:24 +0530

    feat(device-passcocde-fallback): add support for allow device credentials to ios canAuthenticate

--- ios/SimpleBiometrics.mm
+++ ios/SimpleBiometrics.mm
@@ -10,58 +10,77 @@
 @implementation SimpleBiometrics
 RCT_EXPORT_MODULE()
 
-RCT_REMAP_METHOD(canAuthenticate,
-                 canAuthenticateWithResolver:(RCTPromiseResolveBlock)resolve
-                 rejecter:(RCTPromiseRejectBlock)reject)
-{
-    LAContext *context = [[LAContext alloc] init];
-    NSError *la_error = nil;
-    BOOL canEvaluatePolicy = [context canEvaluatePolicy:LAPolicyDeviceOwnerAuthenticationWithBiometrics error:&la_error];
+RCT_REMAP_METHOD(canAuthenticate, allowDeviceCredentials
+                 : (BOOL)allowDeviceCredentials canAuthenticateWithResolver
+                 : (RCTPromiseResolveBlock)resolve rejecter
+                 : (RCTPromiseRejectBlock)reject) {
+  LAContext *context = [[LAContext alloc] init];
+  NSError *la_error = nil;
 
-    if (canEvaluatePolicy) {
-        resolve(@(YES));
-    } else {
-        resolve(@(NO));
-    }
+  LAPolicy localAuthPolicy =
+      allowDeviceCredentials
+          // LAPolicyDeviceOwnerAuthentication allows authentication using
+          // biometrics (Face ID/Touch ID) or device passcode.
+          // If biometry is available, enrolled, and not disabled, the system
+          // uses that first. When these options aren’t available, the system
+          // prompts the user for the device passcode or user’s password.
+          ? LAPolicyDeviceOwnerAuthentication
+          // LAPolicyDeviceOwnerAuthenticationWithBiometrics policy evaluation
+          // fails if Touch ID or Face ID is unavailable or not enrolled.
+          : LAPolicyDeviceOwnerAuthenticationWithBiometrics;
+
+  BOOL canEvaluatePolicy = [context canEvaluatePolicy:localAuthPolicy
+                                                error:&la_error];
+
+  if (canEvaluatePolicy) {
+    resolve(@(YES));
+  } else {
+    resolve(@(NO));
+  }
 }
 
-RCT_REMAP_METHOD(requestBioAuth,
-                 title:(NSString *)title
-                 subtitle:(NSString *)subtitle
-                 requestBioAuthWithResolver:(RCTPromiseResolveBlock)resolve
-                 rejecter:(RCTPromiseRejectBlock)reject)
-{
+RCT_REMAP_METHOD(requestBioAuth, title
+                 : (NSString *)title subtitle
+                 : (NSString *)subtitle requestBioAuthWithResolver
+                 : (RCTPromiseResolveBlock)resolve rejecter
+                 : (RCTPromiseRejectBlock)reject) {
 
-    dispatch_async(dispatch_get_global_queue( DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+  dispatch_async(
+      dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
         NSString *promptMessage = subtitle;
 
         LAContext *context = [[LAContext alloc] init];
         context.localizedFallbackTitle = nil;
 
-        LAPolicy localAuthPolicy = LAPolicyDeviceOwnerAuthenticationWithBiometrics;
+        LAPolicy localAuthPolicy =
+            LAPolicyDeviceOwnerAuthenticationWithBiometrics;
         if (![[UIDevice currentDevice].systemVersion hasPrefix:@"8."]) {
-            localAuthPolicy = LAPolicyDeviceOwnerAuthentication;
+          localAuthPolicy = LAPolicyDeviceOwnerAuthentication;
         }
 
-        [context evaluatePolicy:localAuthPolicy localizedReason:promptMessage reply:^(BOOL success, NSError *biometricError) {
-            if (success) {
-                resolve( @(YES));
+        [context evaluatePolicy:localAuthPolicy
+                localizedReason:promptMessage
+                          reply:^(BOOL success, NSError *biometricError) {
+                            if (success) {
+                              resolve(@(YES));
 
-            } else {
-                NSString *message = [NSString stringWithFormat:@"%@", biometricError.localizedDescription];
-                reject(@"biometric_error", message, nil);
-            }
-        }];
-    });
-
+                            } else {
+                              NSString *message = [NSString
+                                  stringWithFormat:@"%@",
+                                                   biometricError
+                                                       .localizedDescription];
+                              reject(@"biometric_error", message, nil);
+                            }
+                          }];
+      });
 }
 
 // Don't compile this code when we build for the old architecture.
 #ifdef RCT_NEW_ARCH_ENABLED
 - (std::shared_ptr<facebook::react::TurboModule>)getTurboModule:
-    (const facebook::react::ObjCTurboModule::InitParams &)params
-{
-    return std::make_shared<facebook::react::NativeSimpleBiometricsSpecJSI>(params);
+    (const facebook::react::ObjCTurboModule::InitParams &)params {
+  return std::make_shared<facebook::react::NativeSimpleBiometricsSpecJSI>(
+      params);
 }
 #endif
 
